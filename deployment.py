b47a380bfd9d4f59f3dc5754cf646a8298d9a119
Pranavi1609 <112384628+Pranavi1609@users.noreply.github.com>
2023-09-08 15:32:58 +0530

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -47,7 +47,7 @@ def lemmatizer(text):
 
 st.title("Sentiment Analysis App")
 st.markdown("By pranavi bahadursha")
-image = Image.open("2979847c-c5a4-458a-ac3e-6e05dc28091a.png")
+image = Image.open("Sentiment-Analysis1.jpg.webp")
 st.image(image, use_column_width=True)
 st.subheader("Enter your text here:")
 user_input = st.text_area("")
